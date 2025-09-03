b11df72b5d5cfb84db3403b57ecf2e5cabeaf4e9
Ca5parAD <github.unearned388@passmail.net>
2025-09-03 12:54:59 +0100

    improved notification handling - disable and change ntoification fix

--- src/notification_handling.py
+++ src/notification_handling.py
@@ -11,15 +11,21 @@ logger = logging.getLogger(__name__)
 
 def set_notifications(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info("set notification")
+    if context.user_data.get('notification_job'):
+        context.user_data['notification_job'].schedule_removal()
+        del context.user_data['notification_job'] # Clean up notification job
+
     if context.user_data['send_notifications_flag']:
         interval_s = context.user_data['notification_frequency'] * 5
-        context.job_queue.run_repeating(
+        notification_job = context.job_queue.run_repeating(
             send_notification,
             chat_id=update.effective_message.chat_id,
             interval=interval_s,
             first=interval_s,
             data=context.user_data
         )
+        context.user_data['notification_job'] = notification_job # Store job in user data
+
         
 
 
@@ -30,7 +36,7 @@ async def send_notification(context: ContextTypes.DEFAULT_TYPE):
 
     for i, netuid in enumerate(context.job.data['notification_subnets']):
         netuid_name, netuid_price = get_netuid_info(netuid)
-        message += f"({i}) {netuid_name} -> {netuid_price}\n"
+        message += f"({i+1}) {netuid_name} -> {netuid_price}\n"
 
 
     await context.bot.send_message(chat_id=context.job.chat_id, text=message)
