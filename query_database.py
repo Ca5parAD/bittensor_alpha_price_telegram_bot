55fdf349257830b7cef7f7e62942d18f5c17faa8
Ca5parAD <github.unearned388@passmail.net>
2025-10-16 17:32:11 +0800

    Query database script

--- query_database.py
+++ query_database.py
@@ -2,13 +2,14 @@ import sqlite3
 
 from src.config import DATABASE_FILE
 
-path_to_database = DATABASE_FILE
-conn = sqlite3.connect(path_to_database)
+conn = sqlite3.connect(DATABASE_FILE)
 c = conn.cursor()
 
 c.execute("SELECT * FROM user_settings")
-
 users = c.fetchall()
 
 for user in users:
-    print(user)
+    print(f"User: {user[0]}")
+    print(f"Notifications enabled: {"yes" if user[1] else "no"}")
+    print(f"Subnets: {user[2]}")
+    print(f"Notification intervals: {user[3]}hrs\n")
